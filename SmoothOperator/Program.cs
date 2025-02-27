7dd3cd0d2b97c611b4e6b5d23c2587a778d9e833
MiguelPereira-a22411261 <miguel.pereirawork08@gmail.com>
2025-02-27 21:23:42 +0000

    Incremento e decremento

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Insere número inteiro: ");
+            int input = int.Parse(Console.ReadLine());
+            sbyte n = (sbyte)input;
+
+            n--;
+            Console.WriteLine(n);
+            n++;
+            Console.WriteLine(n);
+        }
+    }
+}
