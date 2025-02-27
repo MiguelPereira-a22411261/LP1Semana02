06d29722a9da609876856d99e78203a27019e41e
MiguelPereira-a22411261 <miguel.pereirawork08@gmail.com>
2025-02-27 21:42:04 +0000

    Base

---
+++ Cilindro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Cilindro
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Insira a altura do cilindro (a): ");
+            double altura = double.Parse(Console.ReadLine());
+            
+            
+        }
+    }
+}
