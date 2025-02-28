3124be6c9378f76d97616030c9e493d936cb8ab2
MiguelPereira-a22411261 <miguel.pereirawork08@gmail.com>
2025-02-28 18:02:59 +0000

    Programa finalizado

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -12,14 +12,27 @@ namespace GuessTheNumber
             int count = 0;
 
             bool running = true;
+            bool runningTwo = true;
 
             while(running)
             {    
                 Console.WriteLine("Insert number: ");
                 string playerGuess = Console.ReadLine();
                 count++;
-                int convertGuess = Int32.Parse(playerGuess);
-                
+                int convertGuess = int.Parse(playerGuess);
+                while(runningTwo)
+                {
+                    if (convertGuess < 0 | convertGuess > 30)
+                        Console.WriteLine("Numbers must be between 0 and 30. Try again.");
+                    else if (convertGuess < numberToGuess)
+                        Console.WriteLine("The hidden number is higher than " + convertGuess + ". Try again.");
+                    else if (convertGuess > numberToGuess)
+                        Console.WriteLine("The hidden number is lower than " + convertGuess + ". Try again.");
+                    else
+                        Console.WriteLine("You found the hidden number " + numberToGuess + " after " + count + " tries.");
+                    break;
+                }       
+                break;
             }
 
             
